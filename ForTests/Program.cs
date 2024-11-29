8df3dc603886e70184bc5b794d7def41674d68ee
Петр Брякин <petr.bryakin@gmail.com>
2024-11-29 12:00:52 +0300

    clear main

--- ForTests/Program.cs
+++ ForTests/Program.cs
@@ -7,18 +7,7 @@ namespace ForTests
     {
         static void Main(string[] args)
         {
-            ChocolateRepository repository = new ChocolateRepository();
-            ChocolateDto data = new ChocolateDto()
-            {
-                Name="Cherry Jam",
-                Cost=190,
-                ProductDate="25.03.24",
-                BestBefore=3,
-                Weight=80,
-                CompanyId=2,
-                TypeId=1
-            };
-            repository.AddChocolate(data);
+            
         }
     }
 }
