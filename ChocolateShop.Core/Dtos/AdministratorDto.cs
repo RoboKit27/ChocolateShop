d7da70cc618970138ef4cebb5adf29d73d1ed592
AllFox <tigraneganan1@gmail.com>
2024-12-12 00:44:33 +0300

    Edit AdministratorDto and ManagerDto

--- ChocolateShop.Core/Dtos/AdministratorDto.cs
+++ ChocolateShop.Core/Dtos/AdministratorDto.cs
@@ -8,5 +8,9 @@ namespace ChocolateShop.Core.Dtos
 {
     internal class AdministratorDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Gmail { get; set; }
     }
 }
--- ChocolateShop.Core/Dtos/ManagerDto.cs
+++ ChocolateShop.Core/Dtos/ManagerDto.cs
@@ -8,5 +8,9 @@ namespace ChocolateShop.Core.Dtos
 {
     internal class ManagerDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Gmail { get; set; }
     }
 }
