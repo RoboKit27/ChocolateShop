72ad5bd71498c0bfa2f0059f516535cd5376128e
AllFox <tigraneganan1@gmail.com>
2024-12-12 20:11:40 +0300

    edit Manager & Administrator Dtos and AdministratorRepository

--- ChocolateShop.Core/Dtos/ManagerDto.cs
+++ ChocolateShop.Core/Dtos/ManagerDto.cs
@@ -10,7 +10,7 @@ namespace ChocolateShop.Core.Dtos
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Phone { get; set; }
-        public string Gmail { get; set; }
+        public string? Phone { get; set; }
+        public string? Gmail { get; set; }
     }
 }
--- ChocolateShop.DAL/AdministratorRepository.cs
+++ ChocolateShop.DAL/AdministratorRepository.cs
@@ -8,5 +8,9 @@ namespace ChocolateShop.DAL
 {
     internal class AdministratorRepository
     {
+        public void AdministratorCreateManagers(int managerId, string managerName, string? managerGmail, string? managerPhone)
+        {
+            var manager = new ManagerRepository();
+        }
     }
 }
