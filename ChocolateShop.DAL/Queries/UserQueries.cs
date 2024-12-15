2c2fc34dc33dc2079cd84e186a33c44fa63e2869
AllFox <tigraneganan1@gmail.com>
2024-12-15 22:00:41 +0300

    Delete ManagerRepo, create and edit User Dto, Queries and Repository

---
+++ ChocolateShop.DAL/Queries/UserQueries.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocolateShop.DAL.Queries
+{
+    internal class UserQueries
+    {
+        public const string AddUserQuery = "insert into \"User\"(\"Name\",\"Phone\") values(@Name, @Phone)";
+        public const string GetAllUsersQuery = "select \"Id\", \"Name\", \"Phone\" from \"User\";";
+        public const string GetUserByIdQuery = "select \"Id\", \"Name\", \"Phone\" from \"User\" where \"Id\"=@Id;";
+    }
+}
---
+++ ChocolateShop.DAL/UserRepository.cs
@@ -0,0 +1,59 @@
+﻿using ChocolateShop.Core;
+using ChocolateShop.Core.Dtos;
+using ChocolateShop.DAL.Queries;
+using Dapper;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+namespace ChocolateShop.DAL
+{
+    internal class UserRepository
+    {
+        public List<UserDto> GetAllUsers()
+        {
+            using (var connection = new NpgsqlConnection(Options.ConnectionString))
+            {
+                connection.Open();
+
+                string query = UserQueries.GetAllUsersQuery;
+                List<UserDto> result = connection.Query<UserDto>(query).ToList();
+
+                return result;
+            }
+        }
+        public UserDto GetUserById(int UserId)
+        {
+            using (var connection = new NpgsqlConnection(Options.ConnectionString))
+            {
+                connection.Open();
+
+                string query = UserQueries.GetUserByIdQuery;
+                var param = new { Id = UserId };
+                UserDto result = connection.QueryFirst<UserDto>(query, param);
+
+                return result;
+            }
+        }
+        public void AddUser(UserDto user)
+        {
+            using (var connection = new NpgsqlConnection(Options.ConnectionString))
+            {
+                connection.Open();
+
+                string query = UserQueries.AddUserQuery;
+                var param = new
+                {
+                    Name = user.Name,
+                    Phone = user.Phone
+                };
+                connection.Query(query, param);
+
+            }
+        }
+    }
+}
