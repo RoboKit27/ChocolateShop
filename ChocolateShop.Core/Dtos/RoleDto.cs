17dd6afeda3c4d0de81426ab47aef26d198a09e2
AllFox <tigraneganan1@gmail.com>
2024-12-16 19:44:15 +0300

    Create RoleDto

---
+++ ChocolateShop.Core/Dtos/RoleDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocolateShop.Core.Dtos
+{
+    public class RoleDto
+    {
+        public string? Name { get; set; }
+        public int Id { get; set; }
+    }
+}
